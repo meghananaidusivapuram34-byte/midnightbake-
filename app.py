154cba45534f6ae30ff2fd18d738994dfe61c509
meghananaidusivapuram34-byte <meghananaidusivapuram34@gmail.com>
2026-06-16 13:38:56 +0530

    Initialize Streamlit app for MidnightBake

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="MidnightBake",
+    page_icon="🍰"
+)
+
+st.title("🍰 MidnightBake")
+
+st.subheader("Save Food. Save Money.")
+
+st.write("""
+Welcome to MidnightBake
+
+Buy premium bakery products
+at huge discounts before closing time.
+""")
